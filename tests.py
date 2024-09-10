985f68a6cc7bfe862d5ab4d0be8c672d1794a382
RaimondsPolis <149487123+RaimondsPolis@users.noreply.github.com>
2024-09-10 12:33:40 +0300

    nod1.pabeigts

--- tests.py
+++ tests.py
@@ -1,49 +1,33 @@
 
-
-x = 0
-y=0
-z=0
-
-
-for i in range(0, 5):
-    for j in range (0, 5):
-        print(j *'*')
-        print(i*'*')
-
-
-
-
-
-
-
-
-
-
-
+class Cilveks:
+    def __init__(self, vards, vecums, dzimums):
+        self.name = vards
+        self.gender = dzimums
+        self.age = vecums
+        
+    def dzdiena(self):
+        self.age += 1
+        
+    def vardamaina(self):
+        self.name = input("Ievadi jauno vārdu!")
+        
+    def pastastiparsevi(self):
+        if self.gender == "s":
+            self.gender = "sieviete"
+        elif self.gender == "v":
+            self.gender = "vīrietis"
+        print("sveiki,mani sauc {}, mans dzimums ir {}, man ir {} gadi.".format(self.name, self.gender, self.age))
+        
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+persona = Cilveks("Marta", 34 , "s")
+print(persona.name)
+print(persona.age)
+print(persona.gender)
+raimonds = Cilveks("raimonds", 17, "v")
+print(raimonds.name, raimonds.age, raimonds.gender)
+raimonds.dzdiena()
+print(raimonds.age)
+raimonds.pastastiparsevi()
+raimonds.vardamaina()
+raimonds.pastastiparsevi() 
